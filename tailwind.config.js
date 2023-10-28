2c3d695a1fa47b74beed928235426261752bc645
Dercio João <derciojdsd@gmail.com>
2023-10-28 14:07:01 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,33 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    screens: {
+      mobile: '398px',
+      tablet: '736px',
+      desktop: '1680px'
+    },
+    colors: {
+      white: '#ffffff',
+      gray: '#909194',
+      black: '#242833',
+      'royal-blue': {
+        100: '#EBF1FF',
+        200: '#D5E2FF',
+        400: '#5F8BFF',
+        500: '#3A6FEC',
+        900: '#00123D',
       },
+      mirage: {
+        800: '#282D3A',
+        850: '#1C212C',
+        900: '#131825',
+        950: '#0C0F17',
+      }
+    },
+    fontFamily: {
+      inter: 'Inter, sans-serif'
+    },
+    extend: {
     },
   },
   plugins: [],
