14e11e89df1375b6fff288a1f63c85d819467cbe
Erivelton da Silva <eriveltondasilva13@gmail.com>
2023-10-31 01:03:59 -0300

    feat: create button component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Button from '@/components/Button';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
+      <Button color=''>texto do botão</Button>
       <div>Olá, mundo!</div>
     </main>
-  )
+  );
 }
